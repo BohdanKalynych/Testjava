5be5fc8f409d1ad861cc86dc511be547c1812cdf
Bohdan Kalynych <kalynychbohdan@gmail.com>
2018-03-01 23:22:37 +0200

    Questions: 1) Чому ігнорується зчитування значення SetSongStyle? 2) Чому не рахується TotalLenght? java.util.InputMismatchException 3) Як викликати конкретний параметр об'єкта з ArrayList?

--- Main.java
+++ Main.java
@@ -12,19 +12,20 @@ public class Main {
         // adding objects to ArrayList
         ArrayList<Song> SongList = new ArrayList();
 
-        Double TotalLenght;
+        Double TotalLenght = null;
         for (int i = 1; i < 3; i++) {
             Song xx = new Song();
             System.out.println("Please enter Band and name of the " + i + " song");
             xx.setSongName(sc.nextLine());
             System.out.println("Please enter lenght of the " + i + " song");
             xx.setSongLenght(sc.nextDouble());
-            //TotalLenght = TotalLenght + xx.
+            TotalLenght = TotalLenght + xx.getSongLenght();
             System.out.println("Please enter music style of the " + i + " song");
             xx.setSongStyle(sc.nextLine());
 
             SongList.add(xx);
         }
+
 
         Song Sng = new Song();
         // display ArrayList of entered song information
@@ -35,12 +36,14 @@ public class Main {
             System.out.println(Sng.getFullSongInfo());
         }
 
+        System.out.println("The total lenght of entered songs is: " + TotalLenght);
+
         // Total lenght of entered songs
-        for (int k = 0; k < SongList.size(); k++) {
+        /*for (int k = 0; k < SongList.size(); k++) {
             Sng = SongList.get(k);
             System.out.println(Sng.getFullSongInfo());
             System.out.println("The total lenght of entered songs is:");
 
-        }
+        }*/
     }
 }
